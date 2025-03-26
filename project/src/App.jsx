9cf6cb30ce77c0fc7f6edc0154df6567ec810d00
harshi <harshetab6@gmail.com>
2025-03-27 02:07:51 +0530

    commit2-landing-and-userinput-page

---
+++ project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import ReactDOM from 'react-dom/client';
+import {createBrowserRouter } from 'react-router-dom';
+import TwoNameInputPage from './assets/TwoNameInput';
+const router = createBrowserRouter([]);
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div>site-name</div>
+      </div>
+      <div>
+        <div>2 Player</div>
+        <div>3 Player</div>
+        <div>4 Player</div>
+      </div>
+    </>
+  )
+}
+
+export default App
