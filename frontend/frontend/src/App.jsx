0e2c6d02b6a2aa7d03626d405e1cf3c5d5deef53
harshi <harshetab6@gmail.com>
2025-03-27 23:58:18 +0530

    new folder

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from "react";
+import "./App.css";
+import ReactDOM from "react-dom/client";
+
+// import TwoNameInputPage from './assets/TwoNameInput';
+// const router = createBrowserRouter([]);
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <div className="bg-black">site-name</div>
+      </div>
+      <div>
+        <div>2 Player</div>
+        <div>3 Player</div>
+        <div>4 Player</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
