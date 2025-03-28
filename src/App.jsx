3c6f4c18c47385ff07f167f19affefdc9e70f508
harshi <harshetab6@gmail.com>
2025-03-29 01:40:27 +0530

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import React from "react";
 
 function App() {
   return (
-    <div className="bg-[#1E1B1B] min-h-screen w-screen text-white flex flex-col items-center justify-center">
-      <h1 className="text-3xl mb-4">campus code</h1>
+    <div className="bg-[#1E1B1B] min-h-screen w-screen text-[#ECE6C2] flex flex-col items-center justify-center">
+      <h1 className="text-[70px] mb-10">campus code</h1>
 
-      <div className="flex flex-col gap-4">
-        <Link to="/2player" className="bg-[#73BDA8] px-4 py-2 rounded">
+      <div className="flex gap-15 mb-30 text-[35px]">
+        <Link to="/2player" className="bg-[#73BDA8] px-20 py-10 rounded-[38px]">
           2 Player
         </Link>
-        <Link to="/3player" className="bg-[#CC6B49] px-4 py-2 rounded">
+        <Link to="/3player" className="bg-[#CC6B49] px-20 py-10 rounded-[38px]">
           3 Player
         </Link>
-        <Link to="/4player" className="bg-[#D2A24C] px-4 py-2 rounded">
+        <Link to="/4player" className="bg-[#D2A24C] px-20 py-10 rounded-[38px]">
           4 Player
         </Link>
       </div>
